4b149fb9b2eb03de416e24c07993ebe027b8ff05
KayllonDouglas <kayllon.doug10@gmail.com>
2023-08-24 17:59:38 -0300

    Hmm

--- src/main/java/org/aleta/handler/PacketHandler.java
+++ src/main/java/org/aleta/handler/PacketHandler.java
@@ -1,23 +1,17 @@
 package org.aleta.handler;
 
-import java.nio.charset.StandardCharsets;
-
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+
 import io.netty.channel.socket.DatagramPacket;
+
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class PacketHandler extends SimpleChannelInboundHandler<DatagramPacket> {
 
-    /**
-     * Login Packets
-     * All packets start with a single byte that identifies the packet type, the rest of the packet follows it. Please note that packets 0x09 through 0x13 are not documented (yet).
-     */
-
-    /**
-     * Magic
-     */
     private static final byte[] OFFLINE_MESSAGE_DATA_ID = {
             0x00,
             (byte) 0xff,
@@ -37,66 +31,40 @@ public class PacketHandler extends SimpleChannelInboundHandler<DatagramPacket> {
             0x78
     };
 
-    /**
-     * Packet ID	Field Name	  Field Type	Example	            Notes
-     * 0x01	        Ping ID	      int64	        0x00000000003c6d0d	Time since start in Milliseconds
-     *              MAGIC	      MAGIC
-     * Total Size:	25 Bytes
-     */
-    private static final byte ID_CONNECTED_PING_OPEN_CONNECTIONS = 0x01;
+    private static final byte ID_UNCONNECTED_PING = 0x01;
 
-    private static final byte ID_OPEN_CONNECTION_REQUEST_1 = 0x05;
+    private static final byte ID_OPEN_CONNECTION_REQUEST_ONE = 0x05;
 
-    /**
-     * Packet ID	Field Name	   Field Type	Example	                     Notes
-     * 0x1C	        Ping ID	       int64	    0x00000000003c6d0d	         Time since start in Milliseconds
-     *              Server ID	   int64	    0x00000000372cdc9e	
-     *              MAGIC	       MAGIC		
-     *              Identifier	   string	    "MCPE;Steve;2 7;0.11.0;0;20" Used to send the username, format: MCPE;<Server name>;<Protocol version>;<MCPE Version>;<Players>;<Max Players>
-     *               
-     * Total Size:	35 Bytes + Server name length
-     */
-    private static final byte ID_UNCONNECTED_PING_OPEN_CONNECTIONS = 0x1C;
-
-    /**
-     * Types
-     *        Size	Range	                            Notes
-     * byte	  1	    -128 to 127	                        Signed, two's complement
-     * short  2	    -32768 to 32767	                    Signed, two's complement
-     * int32  4	    -2147483648 to 2147483647           Signed, two's complement
-     * int64  8		Maybe a double?
-     * MAGIC  16	0x00ffff00fefefefefdfdfdfd12345678	always those hex bytes, corresponding to RakNet's default OFFLINE_MESSAGE_DATA_ID
-     * string =1	N/A	                                Prefixed by a short containing the length of the string in characters. It appears that only the following ASCII characters can be displayed: !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~
-     */
+    private static final byte ID_UNCONNECTED_PONG = 0x1C;
 
     @Override
     protected void channelRead0(final ChannelHandlerContext context, final DatagramPacket message) throws Exception {
-        var buffer = message.content();
-        var packetId = buffer.readByte();
 
-        switch(packetId) {
-            case ID_CONNECTED_PING_OPEN_CONNECTIONS -> {
-                log.info("Received ID_CONNECTED_PING_OPEN_CONNECTIONS");
-                var pingId = buffer.readLongLE();
-                var magic = buffer.readBytes(OFFLINE_MESSAGE_DATA_ID.length);
-                log.info("Ping ID: {}", pingId);
-                log.info("Magic: {}", magic);
-                log.info("Sending ID_UNCONNECTED_PONG_OPEN_CONNECTIONS");
-                buffer.writeByte(ID_UNCONNECTED_PING_OPEN_CONNECTIONS);
-                buffer.writeLongLE(pingId);
-                buffer.writeLongLE(0);
-                buffer.writeBytes(OFFLINE_MESSAGE_DATA_ID);
-                buffer.writeCharSequence("MCPE;Aleta;113 113;1.1.5;0;20", StandardCharsets.UTF_8);
+        var buf = message.content();
+
+        switch (buf.readByte()) {
+            case ID_UNCONNECTED_PING -> {
+                log.info("UNCONNECTED_PING from {}", message.sender());
+                var time = buf.readLong();
+                var magic = buf.readBytes(OFFLINE_MESSAGE_DATA_ID.length);
+                var clientGuid = buf.readLong();
+                log.info(" Time: {}, Magic: {}, Client GUID: {}", time, ByteBufUtil.hexDump(magic), clientGuid);
                 break;
             }
-            case ID_OPEN_CONNECTION_REQUEST_1 -> {
+            case ID_OPEN_CONNECTION_REQUEST_ONE -> {
+                log.info("OPEN_CONNECTION_REQUEST_ONE from {}", message.sender());
+                var magic = buf.readBytes(OFFLINE_MESSAGE_DATA_ID.length);
+                var protocol = buf.readByte();
+                var mtu = buf.alloc().buffer(46 + (1432));
+                log.info(" Magic: {}, Protocol: {}, Mtu: {}", ByteBufUtil.hexDump(magic), protocol, mtu);
                 break;
             }
             default -> {
-                log.info("Received unknown packet with id: {}", packetId);
+                log.info("Unknown packet ID: {}", buf.readByte());
+                break;
             }
-
         }
+
     }
 
 }
